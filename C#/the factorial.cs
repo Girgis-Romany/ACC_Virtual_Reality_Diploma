7561662f8f80772e7d1c1e1745db03a4f7481ed3
Gerges Romany <clook550@gmail.com>
2018-05-09 16:57:52 +0200

    Add C# factorial File

---
+++ C#/the factorial.cs
@@ -0,0 +1,32 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication_tables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ACC VR & AR Diploma
+            // Gerges Romany
+            int solution=1;
+            int x;
+             for(x=5;x>0;x--){
+                 solution = solution * x;
+            }
+
+            console.WriteLine(solution);
+            }
+            sleep(500);
+        }
+
+        private static void sleep(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
